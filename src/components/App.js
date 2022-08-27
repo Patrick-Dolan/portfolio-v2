7ddfb5a3359615206ef09cdafa7ecd3a1c4c2516
Patrick Dolan <dolanp1992@gmail.com>
2022-08-27 13:57:08 -0700

    feature: add ThemeProvider to app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,21 @@
 import Navbar from "./navbar/Navbar"
 import { Grid, Typography } from "@mui/material"
+import { ThemeProvider } from "@mui/material";
+import { theme } from "../Theme/index";
 
 function App() {
   return (
     <>
-    <Grid container>
-        <Navbar />
-      <Grid item>
+    <ThemeProvider theme={theme}>
+      <Grid container>
+          <Navbar />
+        <Grid item>
+        </Grid>
+        <Grid item>
+          <Typography variant="h3" >App Component</Typography>
+        </Grid>
       </Grid>
-      <Grid item>
-        <Typography variant="h3" >App Component</Typography>
-      </Grid>
-    </Grid>
+    </ThemeProvider>
     </>
   );
 }
