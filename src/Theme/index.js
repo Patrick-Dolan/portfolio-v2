584540551f1ba5f5c238ca21ed6a46d8a9aa8b57
Patrick Dolan <dolanp1992@gmail.com>
2022-08-27 13:56:32 -0700

    update: add theme object with custom palette

---
+++ src/Theme/index.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#000000',
+      light: '#062f4f',
+    },
+    secondary: {
+      main: '#b82601',
+    },
+  },
+});
