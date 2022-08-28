695e067ffb8bc2556e8f7c36fe26d9bfe3ba329d
Patrick Dolan <dolanp1992@gmail.com>
2022-08-28 10:45:59 -0700

    feature: add skills component with basic container and header

---
+++ src/components/page/Skills.js
@@ -0,0 +1,13 @@
+import { Container, Grid, Typography } from "@mui/material";
+
+const Skills = () => {
+  return (
+    <>
+      <Container maxWidth="xl">
+        <Typography variant="h4" sx={{textAlign: "center", mt: "2em"}}>Skills</Typography>
+      </Container>
+    </>
+  )
+}
+
+export default Skills;
