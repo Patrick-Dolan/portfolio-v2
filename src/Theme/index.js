a21345b6397d39cb95213962ce75bb8d9ed85768
Patrick Dolan <dolanp1992@gmail.com>
2022-08-27 20:52:15 -0700

    update: add tertiary colors, contrastText colors, and change primary's main color

--- src/Theme/index.js
+++ src/Theme/index.js
@@ -4,11 +4,17 @@ export const theme = createTheme({
   palette: {
     type: 'light',
     primary: {
-      main: '#000000',
+      main: '#062f4f',
       light: '#062f4f',
+      contrastText: '#fff'
     },
     secondary: {
       main: '#b82601',
+      contrastText: '#fff'
     },
+    tertiary: {
+      main: '#813772',
+      contrastText: '#fff'
+    }
   },
 });
