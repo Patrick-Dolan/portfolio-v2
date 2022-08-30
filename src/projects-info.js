8b11e22bc8f260e75bfe353584a938ed1b33dc5b
Patrick Dolan <dolanp1992@gmail.com>
2022-08-30 09:58:43 -0700

    update: add image links from github repos

--- src/projects-info.js
+++ src/projects-info.js
@@ -3,7 +3,7 @@ export const droneAttack = {
   description: "A Unity VR game inspired by Pistolwhip made for the Oculus Quest 2 where you shoot drones while moving through a scene dodging enemy return fire.",
   githubLink: "https://github.com/Patrick-Dolan/drone-attack",
   liveLink: null,
-  imgLocation: null,
+  imgLocation: "https://raw.githubusercontent.com/Patrick-Dolan/drone-attack/main/README_IMGS/DroneAttackThumbnail.jpg",
   imgAltText: "Drone Attack VR game."
 }
 
@@ -12,7 +12,7 @@ export const catfish = {
   description: "A C# MVC website built with Razor, Entity, Identity, Migrations, Jquery, and Animate.css. This is a tinder-like website for pairing pets for playdates. It includes full CRUD on user profiles and a matching system that favors showing you opportunities to match with pets who have already made a favorable choice when seeing your profile.",
   githubLink: "https://github.com/Patrick-Dolan/Catfish.Solution",
   liveLink: null,
-  imgLocation: null,
+  imgLocation: "https://github.com/Patrick-Dolan/Catfish.Solution/raw/main/README_IMAGES/Previews/GeneralPreview.gif",
   imgAltText: "CatFish website."
 }
 
@@ -21,6 +21,6 @@ export const pierresMarket = {
   description: "A C# MVC website built with Razor, Entity, Identity, and Migrations. You can log into the site to add treats and flavor tags. You can view treats and their corresponding flavors whether logged in or not but you cant add or delete them.",
   githubLink: "https://github.com/Patrick-Dolan/PierresMarket.Solution",
   liveLink: null,
-  imgLocation: null,
+  imgLocation: "https://raw.githubusercontent.com/Patrick-Dolan/PierresMarket.Solution/main/DemoGifs/BasicFunctionality.gif",
   imgAltText: "Pierres Market website."
 }
