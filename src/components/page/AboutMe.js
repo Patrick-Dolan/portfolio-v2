7f199afaf3422abd4ac7598a2c6d896b3117cd10
Patrick Dolan <dolanp1992@gmail.com>
2022-08-30 10:15:37 -0700

    feature: create basic AboutMe component

---
+++ src/components/page/AboutMe.js
@@ -0,0 +1,13 @@
+import { Container, Typography } from "@mui/material";
+
+const AboutMe = () => {
+  return(
+    <>
+      <Container maxWidth="xl" sx={{py: "4em"}}>
+        <Typography variant="h4">About Me</Typography>
+      </Container>
+    </>
+  )
+}
+
+export default AboutMe;
