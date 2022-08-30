5df5927670343616c160a3025c09621becf52c41
Patrick Dolan <dolanp1992@gmail.com>
2022-08-30 08:20:18 -0700

    feature: add projects-info to act as local database

---
+++ src/projects-info.js
@@ -0,0 +1,26 @@
+export const droneAttack = {
+  title: "Drone Attack",
+  description: "A Unity VR game inspired by Pistolwhip made for the Oculus Quest 2 where you shoot drones while moving through a scene dodging enemy return fire.",
+  githubLink: "https://github.com/Patrick-Dolan/drone-attack",
+  liveLink: null,
+  imgLocation: null,
+  imgAltText: "Drone Attack VR game."
+}
+
+export const catfish = {
+  title: "CatFish",
+  description: "A C# MVC website built with Razor, Entity, Identity, Migrations, Jquery, and Animate.css. This is a tinder-like website for pairing pets for playdates. It includes full CRUD on user profiles and a matching system that favors showing you opportunities to match with pets who have already made a favorable choice when seeing your profile.",
+  githubLink: "https://github.com/Patrick-Dolan/Catfish.Solution",
+  liveLink: null,
+  imgLocation: null,
+  imgAltText: "CatFish website."
+}
+
+export const pierresMarket = {
+  title: "Pierres Market",
+  description: "A C# MVC website built with Razor, Entity, Identity, and Migrations. You can log into the site to add treats and flavor tags. You can view treats and their corresponding flavors whether logged in or not but you cant add or delete them.",
+  githubLink: "https://github.com/Patrick-Dolan/PierresMarket.Solution",
+  liveLink: null,
+  imgLocation: null,
+  imgAltText: "Pierres Market website."
+}
