fdd9681a514116c67ffdc29816c333c20288eeb0
Patrick Dolan <dolanp1992@gmail.com>
2022-09-04 18:47:43 -0700

    update: add id for anchor to setup routing

--- src/components/page/ContactMe.js
+++ src/components/page/ContactMe.js
@@ -50,75 +50,75 @@ const ContactMe = () => {
   };
   
   return (
-    <>
-    <Container maxWidth="md" sx={{color: theme.palette.primary.contrastText, py: "4em"}}>
-      <Paper sx={{
-        py: "1em", 
-        px: "1em"
-      }}>
-        <Typography variant="h4" textAlign={"center"}>Contact Me</Typography>
-        <form ref={form} onSubmit={sendEmail}>
-          <TextField
-            id="filled-name"
-            label="Name"
-            name="name"
-            margin="normal"
-            
-            fullWidth
-            required
-          />
-          <TextField
-            id="filled-name"
-            label="Email"
-            name="user_email"
-            margin="normal"
-            type="email"
-            fullWidth
-            required
-          />
-          <TextField
-            id="filled-name"
-            label="Message"
-            name="message"
-            margin="normal"
-            multiline
-            minRows={4}
-            maxRows={6}
-            fullWidth
-            required
-          />
-          <Button
-            variant="contained"
-            color="secondary"
-            endIcon={<SendIcon />}
-            type="submit"
-          >
-            Send message
-          </Button>
-        </form>
-      </Paper>
-      <Snackbar 
-        autoHideDuration={4000}
-        message="Your message was successfully sent"
-        open={successOpen}
-        onClose={handleSuccessClose}
-      >
-        <Alert onClose={handleSuccessClose} severity="success" sx={{backgroundColor: "green", color: "white"}}>
-          Your message was successfully sent
-        </Alert>
-      </Snackbar>
-      <Snackbar 
-        autoHideDuration={4000}
-        message="Something went wrong with when sending your message."
-        open={errorOpen}
-        onClose={handleErrorClose}
-      >
-        <Alert onClose={handleErrorClose} severity="error" sx={{backgroundColor: "red", color: "white"}}>
-          Something went wrong with when sending your message.
-        </Alert>
-      </Snackbar>
-    </Container>
-    </>
+    <div id="contactme">
+      <Container maxWidth="md" sx={{color: theme.palette.primary.contrastText, py: "4em"}}>
+        <Paper sx={{
+          py: "1em", 
+          px: "1em"
+        }}>
+          <Typography variant="h4" textAlign={"center"}>Contact Me</Typography>
+          <form ref={form} onSubmit={sendEmail}>
+            <TextField
+              id="filled-name"
+              label="Name"
+              name="name"
+              margin="normal"
+              
+              fullWidth
+              required
+            />
+            <TextField
+              id="filled-name"
+              label="Email"
+              name="user_email"
+              margin="normal"
+              type="email"
+              fullWidth
+              required
+            />
+            <TextField
+              id="filled-name"
+              label="Message"
+              name="message"
+              margin="normal"
+              multiline
+              minRows={4}
+              maxRows={6}
+              fullWidth
+              required
+            />
+            <Button
+              variant="contained"
+              color="secondary"
+              endIcon={<SendIcon />}
+              type="submit"
+            >
+              Send message
+            </Button>
+          </form>
+        </Paper>
+        <Snackbar 
+          autoHideDuration={4000}
+          message="Your message was successfully sent"
+          open={successOpen}
+          onClose={handleSuccessClose}
+        >
+          <Alert onClose={handleSuccessClose} severity="success" sx={{backgroundColor: "green", color: "white"}}>
+            Your message was successfully sent
+          </Alert>
+        </Snackbar>
+        <Snackbar 
+          autoHideDuration={4000}
+          message="Something went wrong with when sending your message."
+          open={errorOpen}
+          onClose={handleErrorClose}
+        >
+          <Alert onClose={handleErrorClose} severity="error" sx={{backgroundColor: "red", color: "white"}}>
+            Something went wrong with when sending your message.
+          </Alert>
+        </Snackbar>
+      </Container>
+    </div>
   )
 }
 
