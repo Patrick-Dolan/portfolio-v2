835ff12d9901b7443d6cbea35de2a37be2b3a798
Patrick Dolan <dolanp1992@gmail.com>
2022-09-04 21:27:26 -0700

    update: update about me text

--- src/components/page/AboutMe.js
+++ src/components/page/AboutMe.js
@@ -12,11 +12,11 @@ const AboutMe = () => {
           </Grid>
           <Grid item md={6}>
             <Typography variant="h4" textAlign={"center"}>About Me</Typography>
-            <Typography>Hello, my name is Patrick Dolan and I love building fun, interactive, polished websites and user experiences. I'm working towards a career change from freelance art and delivery driving to frontend web development. I began this career change in 2021 and I'm looking for work where I can continue to learn and provide the best possible services I can.</Typography>
+            <Typography>Hello, I'm Patrick, and I love building things for the web. I started my adventure as a developer in 2021 but feel like I've been working towards this craft for as long as I can remember. I've been drawn to computers since childhood and have been fascinated with how they work since I played my first video game.</Typography>
             <br />
-            <Typography>Straight out of high school I went into the Information Technology field while attending classes at Clark College for computer support. I enjoyed working with computers and the myriad of problem solving situations they provided but a few years back I decided to shake things up and started working full time as a freelance artist. I learned a ton about self management and business during my time in professional art but It's time for another career change and I'm hoping to merge my love of computers with that of design which is why I enrolled at Epicodus (A full stack software development boot camp).</Typography>
+            <Typography>That fascination is what led me to freelance in computer support and digital art. I've been working in both fields for the past few years building/repairing computers and networks and creating digital art assets for online creators and small businesses.</Typography>
             <br />
-            <Typography>I've been absolutely loving the learning environment at Epicodus and I've learned a ton of different things like HTML, CSS, Javascript, Jest, Git, TDD,and more. I can't wait to start my career in software development where I can work with a team to create experiences users can't get enough of. If you have an opprotunity that you think I could bring value to please feel free to contact me at: dolanp1992@gmail.com or use the social links at the top of the page.</Typography>
+            <Typography>I had dabbled in programming before but until I really threw myself into web development I had never understood how intensely I enjoy building useful websites like the one you will find in this portfolio. I've just recently finished a full stack web development Bootcamp and am currently in my first internship but I'm looking for a junior developer position so I can start my career.</Typography>
           </Grid>
         </Grid>
       </Container>
